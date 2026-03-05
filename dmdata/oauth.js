47d80530d766e017b0dfbd020c17d1df00b5249e
looksky495 (Mac) <tuyatuya_muhco@icloud.com>
2026-03-05 15:52:16 +0900

    認証フロー実装

---
+++ dmdata/oauth.js
@@ -0,0 +1,50 @@
+const SCOPES = [
+  "contract.list",
+  "eew.get.forecast",
+  "eew.get.warning",
+  "parameter.earthquake",
+  "parameter.tsunami",
+  "socket.close",
+  "socket.start",
+  "telegram.data",
+  "telegram.get.earthquake",
+  "telegram.list"
+];
+
+const authServer = {
+  issuer: "https://manager.dmdata.jp/",
+  authorization_endpoint: "https://manager.dmdata.jp/account/oauth2/v1/auth",
+  token_endpoint: "https://manager.dmdata.jp/account/oauth2/v1/token",
+  revocation_endpoint: "https://manager.dmdata.jp/account/oauth2/v1/revoke",
+  code_challenge_methods_supported: ["S256"],
+};
+
+/**
+ * Authenticate with DMData API and get an access token.
+ * @param {string} secretKey
+ * @param {string} clientId
+ */
+export async function auth(secretKey, clientId){
+  if (!secretKey || !clientId){
+    throw new Error("The API secret key and client ID must be provided.");
+  }
+
+  const response = await fetch(authServer.token_endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      "client_id": clientId,
+      "client_secret": secretKey,
+      "grant_type": "client_credentials",
+      "scope": SCOPES.join(" ")
+    })
+  }).then(res => res.json());
+
+  if (response.error){
+    throw new Error("Failed to authenticate: " + response.error_description);
+  }
+
+  return response.access_token;
+}
